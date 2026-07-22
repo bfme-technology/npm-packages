09311110a988e032d4b19ac24f0691b64a3af360
Deepak Belbase <deepak.belbase4@gmail.com>
2026-07-22 13:24:12 +0530

    chore: Add .gitignore and remove node_modules and dist from tracking

--- @bfme-technology/spa/dist/FederatedModule/FederatedModule.d.ts
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import type { FederatedModuleProps } from './FederatedModule.types';
-export declare const FederatedModule: React.FC<FederatedModuleProps>;
-export default FederatedModule;
